97c6aad733b12c26baf12ad5b4c813ceec605614
Katariina Vakkuri <katariina.vakkuri@polarsquad.com>
2022-10-06 13:13:57 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ReactiveButton from 'reactive-button';
+
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function App() {
+  const [todo, setTodo] = useState({description: '', date: '', status: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({description: '', date: '', status: ''});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
+      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
+      <input placeholder="Status" name="status" value={todo.status} onChange={inputChanged}/>
+      <ReactiveButton color="green" idleText="Add"/>
+      <div className="ag-theme-material" style={ { height: 600, width: 600, margin: 'auto' } }>
+  <AgGridReact
+    rowData={todos} animateRows={true}>
+    <AgGridColumn field="description" sortable={true} filter={true} suppressMovable={true}></AgGridColumn>
+    <AgGridColumn field="date" sortable={true} filter={true} suppressMovable={true}></AgGridColumn>
+    <AgGridColumn field="status" sortable={true} filter={true} suppressMovable={true}></AgGridColumn>
+  </AgGridReact>
+</div>
     </div>
   );
 }
